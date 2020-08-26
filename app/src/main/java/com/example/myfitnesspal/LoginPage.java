dc96230d2865b335e590c8b922a731b117594329
Aditya Mishra <adityalmishra25@gmail.com>
2020-08-26 17:59:21 +0530

    dialog box added on login page

--- app/src/main/java/com/example/myfitnesspal/LoginPage.java
+++ app/src/main/java/com/example/myfitnesspal/LoginPage.java
@@ -3,10 +3,13 @@ package com.example.myfitnesspal;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,8 +21,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginPage extends AppCompatActivity {
-    EditText email,password;
-    Button signin;
+    EditText email,password, em1;
+    Button signin, ok, cncl;
     FirebaseAuth fauth;
 
     @Override
@@ -107,21 +110,49 @@ public class LoginPage extends AppCompatActivity {
     }
 
     public void forgotPassword(View view) {
-        EditText email=(EditText) findViewById(R.id.email);
-        String em = email.getText().toString();
-        fauth.sendPasswordResetEmail(em).addOnCompleteListener(new OnCompleteListener<Void>() {
+//        EditText email=(EditText) findViewById(R.id.email);
+//        String em = email.getText().toString();
+        final ViewGroup viewGroup = findViewById(android.R.id.content);
+        final View dialogView = LayoutInflater.from(LoginPage.this).inflate(R.layout.forgot_pass, viewGroup, false);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(LoginPage.this);
+        builder.setView(dialogView);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        ok = dialogView.findViewById(R.id.sendlink);
+        cncl = dialogView.findViewById(R.id.cancel);
+        em1 = dialogView.findViewById(R.id.emaillink);
+        ok.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(LoginPage.this,"loggedin",Toast.LENGTH_SHORT).show();
-                    Log.d("Sucess","logged");
+            public void onClick(View view) {
+                final String emaillink = em1.getText().toString();
+                if(emaillink.equals("")){
+                    Toast.makeText(LoginPage.this,"Please input some value",Toast.LENGTH_LONG).show();
                 }
                 else{
-                    Toast.makeText(LoginPage.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
-                    Log.d("error",task.getException().toString());
+                    fauth.sendPasswordResetEmail(emaillink).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(LoginPage.this,"loggedin",Toast.LENGTH_SHORT).show();
+                                Log.d("Sucess","logged");
+                            }
+                            else{
+                                Toast.makeText(LoginPage.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
+                                Log.d("error",task.getException().toString());
+                            }
+                        }
+                    });
+                    alertDialog.dismiss();//change code here
                 }
             }
         });
+        cncl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+
     }
 }
 
