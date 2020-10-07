419536ad125a70028fb2e40909db2e99adc49f4d
Aditya Mishra <adityalmishra25@gmail.com>
2020-10-08 00:27:21 +0530

    Notification is up and running

--- app/src/main/java/com/example/myfitnesspal/MainActivity.java
+++ app/src/main/java/com/example/myfitnesspal/MainActivity.java
@@ -9,20 +9,82 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference fdb;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpNavigation();
+        firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getUid();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        fdb = firebaseDatabase.getReference("Water").child(uid);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            final NotificationChannel channel = new NotificationChannel("MyNotification","MyNotification", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.enableLights(true);
+            final NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            fdb.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String value = snapshot.child("00 toggle").getValue().toString();
+                    if (value == "True") {
+                        notificationManager.createNotificationChannel(channel);
+                    }
+                    else{
+                        notificationManager.deleteNotificationChannel("MyNotification");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        FirebaseMessaging.getInstance().subscribeToTopic("general")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Successful";
+                        if (!task.isSuccessful()) {
+                            msg = "Failed";
+                        }
+                        Log.d("TAG", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
     }
 
     private void setUpNavigation() {
@@ -30,6 +92,5 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment= (NavHostFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment);
         NavigationUI.setupWithNavController(navigationView,navHostFragment.getNavController());
-
     }
 }
--- app/src/main/java/com/example/myfitnesspal/settings.java
+++ app/src/main/java/com/example/myfitnesspal/settings.java
@@ -1,18 +1,31 @@
 package com.example.myfitnesspal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +36,8 @@ public class settings extends Fragment {
     Button log;
     FirebaseAuth fAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+    Switch notification;
+    DatabaseReference fdb;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -54,6 +69,33 @@ public class settings extends Fragment {
         return fragment;
     }
 
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+//        final SharedPreferences.Editor editor = sharedPref.edit();
+//        fdb.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                String value = snapshot.child("00 toggle").getValue().toString();
+//                if (value == "True"){
+//                    editor.putBoolean("switchValue", true).commit();
+//                    notification.setChecked(true);
+//                }
+//                else{
+//                    editor.putBoolean("switchValue", false).commit();
+//                    notification.setChecked(false);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -71,6 +113,28 @@ public class settings extends Fragment {
                 }
             }
         });
+
+        //notification toggle button
+        String uid=fAuth.getUid();
+        notification = (Switch) getView().findViewById(R.id.toggle);
+        fdb = FirebaseDatabase.getInstance().getReference("Water").child(uid);
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPref.edit();
+
+        notification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(notification.isChecked()){
+                    fdb.child("00 toggle").setValue("True");
+                    editor.putBoolean("switchValue", true).commit();
+                }
+                else{
+                    fdb.child("00 toggle").setValue("False");
+                    editor.putBoolean("switchValue", false).commit();
+                }
+            }
+        });
     }
 
     @Override
@@ -80,13 +144,18 @@ public class settings extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        boolean def = sharedPref.getBoolean("switchValue", false);
+        Switch notification;
+        notification = (Switch) view.findViewById(R.id.toggle);
+        notification.setChecked(def);
+        return view;
     }
 }
